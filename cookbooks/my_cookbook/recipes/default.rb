e3979cf5d44154e626400bd0aa1964c7935c89c2
vagrant <vagrant@vagrant.vm>
2013-11-10 02:04:13 +0000

    add berkshelf

---
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: my_cookbook
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe "chef-client"
+include_recipe "apt"
+include_recipe "ntp"
